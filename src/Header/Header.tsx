11eccd3debb47fe71a46f7d8f3593c887e40f6bb
Mikolaj H <mickydeveloper@gmail.com>
2024-10-16 19:27:55 +0200

    added header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+import "./Header.css";
+import logo from "../icons/logo.svg";
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
+      <Navbar />
+    </header>
+  );
+}
---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="app-navbar">
+      <ul>
+        <li>
+          <a href="#">Main</a>
+        </li>
+        <li>
+          <a href="info">Info</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
